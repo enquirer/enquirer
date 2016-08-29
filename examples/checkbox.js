59362d8d79adc9001c7ebe80f678386a7d34f3db
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-29 18:05:18 -0400

    adds checkbox example, tests

--- examples/checkbox.js
+++ examples/checkbox.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var Separator = require('choices-separator');
 var Enquirer = require('..');
 var enquirer = new Enquirer();
 
@@ -9,32 +8,13 @@ enquirer.register('checkbox', require('enquirer-prompt-checkbox'));
 var questions = [
   {
     type: 'checkbox',
-    message: 'Select toppings',
-    name: 'toppings',
+    message: 'What is your favorite color?',
+    name: 'color',
     choices: [
-      new Separator(' = The Meats = '),
-      {name: 'Pepperoni'},
-      {name: 'Ham'},
-      {name: 'Ground Meat'},
-      {name: 'Bacon'},
-      new Separator(' = The Cheeses = '),
-      {name: 'Mozzarella', checked: true },
-      {name: 'Cheddar'},
-      {name: 'Parmesan'},
-      new Separator(' = The usual ='),
-      {name: 'Mushroom'},
-      {name: 'Tomato'},
-      new Separator(' = The extras = '),
-      {name: 'Pineapple'},
-      {name: 'Olives', disabled: 'out of stock'},
-      {name: 'Extra cheese'}
-    ],
-    validate: function (answer) {
-      if (answer.length < 1) {
-        return 'You must choose at least one topping.';
-      }
-      return true;
-    }
+      'red',
+      'blue',
+      'yellow'
+    ]
   }
 ];
 
---
+++ examples/checkbox.validate.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var Enquirer = require('..');
+var enquirer = new Enquirer();
+
+enquirer.register('checkbox', require('enquirer-prompt-checkbox'));
+
+var separator = enquirer.separator.bind(enquirer);
+var questions = [
+  {
+    type: 'checkbox',
+    message: 'Select toppings',
+    name: 'toppings',
+    choices: [
+      separator(' = The Meats = '),
+      {name: 'Pepperoni'},
+      {name: 'Ham'},
+      {name: 'Ground Meat'},
+      {name: 'Bacon'},
+      separator(' = The Cheeses = '),
+      {name: 'Mozzarella', checked: true },
+      {name: 'Cheddar'},
+      {name: 'Parmesan'},
+      separator(' = The usual ='),
+      {name: 'Mushroom'},
+      {name: 'Tomato'},
+      separator(' = The extras = '),
+      {name: 'Pineapple'},
+      {name: 'Olives', disabled: 'out of stock'},
+      {name: 'Extra cheese'}
+    ],
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return 'You must choose at least one topping.';
+      }
+      return true;
+    }
+  }
+];
+
+enquirer.ask(questions)
+  .then(function(answers) {
+    console.log(answers)
+  });
