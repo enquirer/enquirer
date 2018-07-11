0b66520bd18bbcf93f247caf08a85eb48cc88181
jonschlinkert <jon.schlinkert@sellside.com>
2018-07-11 05:10:26 -0400

    clean up index

--- index.js
+++ index.js
@@ -1,62 +1,9 @@
-// 'use strict';
-
-// const Emitter = require('events');
-// let { prompts } = require('./');
-
-const ignores = ['suggest', 'format', 'validate', 'test'];
-function isIgnored(key, proto) {
-  return (key in proto) || ignores.includes(key) || /^on[A-Z]/.test(key);
-}
-
-// module.exports = (types = prompts) => {
-//   return async(questions = [], options = {}, answers = {}) => {
-//     const ignored = [];
-
-//     for (const question of [].concat(questions)) {
-//       if (question.skip && await question.skip(question, { ...answers })) {
-//         continue;
-//       }
-
-//       const Prompt = types[question.type];
-//       const prompt = new Prompt();
-
-//       for (const key of Object.keys(question)) {
-//         if (isIgnored(key, Prompt.prototype)) {
-//           ignored.push(key);
-//           continue;
-//         }
-
-//         if (typeof question[key] === 'function') {
-//           question[ket] = await question[key].call(prompt, question, { ...answers });
-//         }
-//       }
-
-//       answers[question.name] = await prompt.run();
-
-//       for (const key of ignored) {
-//         await question[key].call(prompt, question, answers);
-//       }
-//     }
-//     return answers;
-//   };
-// };
-
-// module.exports = async(questions, options) => {
-//   const answers = {};
-//   for (const question of [].concat(questions)) {
-//     const opts = { ...options, ...question };
-//     const Prompt = prompts[opts.type];
-//     const prompt = new Prompt(opts);
-//     const answer = await prompt.run();
-//     answers[prompt.name] = answer;
-//   }
-//   return answers;
-// };
-
 'use strict';
 
+const assert = require('assert');
 const Emitter = require('events');
-let prompts = require('./prompts');
+const prompts = require('./prompts');
+const ignores = ['suggest', 'format', 'validate', 'test'];
 
 class Enquirer extends Emitter {
   constructor(options, answers) {
@@ -78,14 +25,27 @@ class Enquirer extends Emitter {
 
   async prompt(questions = [], options) {
     const answers = { ...this.answers };
-    const ignored = [];
+    let prev;
 
     for (const question of [].concat(questions)) {
       if (question.skip && await question.skip(question, { ...answers })) {
         continue;
       }
 
-      const Prompt = this.prompts[question.type];
+      let { type, name } = question;
+      let ignored = [];
+
+
+      if (typeof type === 'function') {
+        type = await type();
+      }
+
+      if (typeof type !== 'string') {
+        continue;
+      }
+
+      assert(this.prompts[type], type + ' is not registered');
+      const Prompt = this.prompts[type];
       const prompt = new Prompt(question);
 
       for (const key of Object.keys(question)) {
@@ -95,22 +55,30 @@ class Enquirer extends Emitter {
         }
 
         if (typeof question[key] === 'function') {
-          question[key] = await question[key].call(prompt, question, { ...answers });
+          question[key] = await question[key].call(prompt, prev, question, { ...answers });
         }
       }
 
-      answers[question.name] = await prompt.run();
+      let answer = answers[name] = await prompt.run();
 
       for (const key of ignored) {
-        await question[key].call(prompt, question, answers);
+        await question[key].call(prompt, answer, question, { ...answers });
       }
+
+      prev = answer;
     }
     return answers;
   }
 }
 
+function isIgnored(key, proto) {
+  return (key in proto) || ignores.includes(key) || /^on[A-Z]/.test(key);
+}
+
 const enquirer = new Enquirer();
 const questions = require('./questions');
+
+enquirer.register('snippet', require('./recipes/snippet'));
 
 enquirer.prompt(questions)
   .then(answers => console.log('ANSWERS:', answers))
