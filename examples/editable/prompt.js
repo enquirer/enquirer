a03147fd4ec536efe0ff19a3f90eae690a58dbc8
Jon Schlinkert <jon.schlinkert@sellside.com>
2018-11-05 18:28:51 -0500

    adds editable prompt
    experimental

---
+++ examples/editable/prompt.js
@@ -0,0 +1,37 @@
+const Prompt = require('../../lib/prompts/editable');
+const prompt = new Prompt({
+  name: 'user',
+  message: 'Please provide the following information:',
+  choices: [
+    {
+      name: 'firstname',
+      message: 'First Name',
+      initial: 'Jon',
+      editable: true,
+      prefix() {
+        return prompt.symbols.check;
+      }
+    },
+    {
+      name: 'lastname',
+      message: 'Last Name',
+      initial: 'Schlinkert',
+      prefix() {
+        return prompt.symbols.check;
+      }
+    },
+    {
+      name: 'username',
+      message: 'GitHub username',
+      initial: 'jonschlinkert',
+      prefix() {
+        return prompt.symbols.check;
+      }
+    }
+  ]
+});
+
+prompt
+  .run()
+  .then(value => console.log('ANSWERS:', value))
+  .catch(console.error);
