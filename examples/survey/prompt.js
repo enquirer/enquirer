a3727ace7bbf04c58e053f854439822cac609a91
Jon Schlinkert <jon.schlinkert@sellside.com>
2018-11-07 03:22:40 -0500

    adds `survey` prompt

---
+++ examples/survey/prompt.js
@@ -0,0 +1,30 @@
+const Prompt = require('../../lib/prompts/survey');
+const prompt = new Prompt({
+  message: 'Please rate your experience',
+  choices: [
+    {
+      name: 'interface',
+      message: 'The website has a friendly interface.'
+    },
+    {
+      name: 'navigation',
+      message: 'The website is easy to navigate.'
+    },
+    {
+      name: 'images',
+      message: 'The website usually has good images.'
+    },
+    {
+      name: 'upload',
+      message: 'The website makes it easy to upload images.'
+    },
+    {
+      name: 'colors',
+      message: 'The website has a pleasing color palette.'
+    }
+  ]
+});
+
+prompt.run()
+  .then(value => console.log('ANSWERS:', value))
+  .catch(console.error);
