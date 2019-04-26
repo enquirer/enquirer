9fb2bc0b6d23367f46d34d8b410bd11bcb7e2ec9
Mehul <318097@gmail.com>
2019-04-26 14:09:46 +0530

    fix: confirm/option-istrue.js
    updated the example to show (t/F) instead of (y/N)

--- examples/confirm/option-istrue.js
+++ examples/confirm/option-istrue.js
@@ -2,7 +2,8 @@ const { Confirm } = require('enquirer');
 
 const prompt = new Confirm({
   name: 'apples',
-  message: 'How do you like these apples?',
+  message: 'Do you like apples?',
+  default: '(t/F)',
   onRun() {
     this.isTrue = value => value === 'f';
     this.isFalse = value => value === 't';
